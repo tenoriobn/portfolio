d8f0da4b378ad9e21172dcd1f5c5c91eb873f5d5
Bruno Tenório <tenoriobn@gmail.com>
2024-01-30 22:23:43 -0300

    Adicionando variavel de ambiente

--- src/common/state/hooks/formulario/useEnviarEmail.ts
+++ src/common/state/hooks/formulario/useEnviarEmail.ts
@@ -9,7 +9,7 @@ const useEnviarEmail = () => {
   const enviarEmail = (data: IDadosFormulario, reset: () => void) => {
     const serviceID = import.meta.env.VITE_REACT_EMAILJS_SERVICE_ID;
     const templateID = import.meta.env.VITE_REACT_EMAILJS_TEMPLATE_ID;
-    const publicKey = import.meta.env.VITE_REACT_EMAILJS_USER_ID;
+    const publicKey = import.meta.env.VITE_REACT_EMAILJS_USER_ID; 
 
     emailjs.send(serviceID, templateID, data, publicKey)
       .then((res) => {
